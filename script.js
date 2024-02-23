cd0e503d3f0652d32aefadb83e9d62909875a905
sadiqhawaldarr <160845812+sadiqhawaldarr@users.noreply.github.com>
2024-02-23 18:43:41 +0530

    Datatypes, Array and objects

---
+++ script.js
@@ -0,0 +1,69 @@
+console.log("hello javascript")
+
+//Variable
+
+//primitive datatype 
+
+let myname = "sadiq "
+lastname = "hawaldar"
+console.log(myname + lastname)
+
+// number
+
+let age = 25
+
+console.log( age + 5)
+
+
+
+// booliean
+
+let workinginTCS = true
+console.log(workinginTCS)
+
+//undefined
+
+let salary
+console.log(salary)
+
+
+//Null
+
+let avalable_leave = null
+console.log(avalable_leave)
+
+//non primitive data type 
+
+//Array
+
+let color = ["red", "blue", "white", "green"]
+console.log(color)
+
+
+//how to access the value fron list
+const students = ['sadiq', 25, "adress", "number"]
+let student = students[3]
+console.log(student)
+
+
+//object
+
+const mydetails = {
+
+myname : "sadiq|_hawaldar",
+age : 25,
+mynumber :7892652144,
+adress : "pune"
+
+}
+console.log(mydetails)
+
+
+//alert
+const person = {
+    firstName: "sameer",
+    lastName : "hawaldar",
+  }
+
+alert(person.firstName)
+
